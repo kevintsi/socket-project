a2f2e77de2337e6ee21d3df428c4ea0d57e6ea73
Kévin TSI <kevtsiii@gmail.com>
2022-02-18 00:01:04 +0100

    Creation of some event and emit (client)

---
+++ client/javascript.js
@@ -0,0 +1,20 @@
+const socket = io("http://localhost:5000");
+
+socket.on("connect", () => {
+    // either with send()
+    socket.send("Hello!");
+    console.log("Connected")
+
+    // or with emit() and custom event names
+    socket.emit("salutations", ["Hello!", { "mr": "john" }, Uint8Array.from([1, 2, 3, 4])]);
+});
+
+// handle the event sent with socket.send()
+socket.on("message", data => {
+    console.log(data);
+});
+
+// handle the event sent with socket.emit()
+socket.on("greetings", (elem1, elem2, elem3) => {
+    console.log(elem1, elem2, elem3);
+});
